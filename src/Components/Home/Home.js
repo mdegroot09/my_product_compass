affc8a7892722083b8aeb6b72d48448c00844737
Mike De Groot <mdegroot09@gmail.com>
2019-05-02 07:50:00 -0600

    refactored homepage layout

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,7 +5,6 @@ export default class Home extends Component {
     return (
       <div className='homeMainDiv'>
         <div className='mainLogoDiv'>
-          {/* <img className='mainLogo' src="https://i.ibb.co/MsMrNvp/Header1.png" alt=""/> */}
           <h1>My Product Compass</h1>
           <button className='getStarted'>get started</button>
         </div>
