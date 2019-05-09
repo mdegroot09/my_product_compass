1fd3a2e465f09b23fd3eab03953c499470e43ba4
Mike De Groot <mdegroot09@gmail.com>
2019-05-09 12:08:48 -0600

    Navbar, Home now responsive to screen size

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -15,42 +15,54 @@ class Header extends Component {
     })
   }
 
+  showMenu(){
+    let hiddenMenus = document.getElementsByClassName('hiddenMenu')
+    let hiddenMenu = hiddenMenus[0]
+    if (hiddenMenu.classList.contains('expand')){
+      hiddenMenu.classList.remove('expand')
+    } else {
+      hiddenMenu.classList.add('expand')
+    }
+  }
+
   render() {
     return(
       <div>
         <header>
           <Router>
-            <div className='headerLeft'>
-              <Link className='headerLogoParent' to='/'>
-                <img className='headerLogo' src="https://i.ibb.co/kcBcSnq/compass-logo-inverted.png" alt="My Product Compass logo"/>
-              </Link>
-              <Link to='/'>
-                <button className='headerBtn'>Home</button>
-              </Link>
-              <Link to='/products'>
-                <button className='headerBtn'>Products</button>
-              </Link>
-              <Link to='/devs'>
-                <button className='headerBtn'>Devs</button>
-              </Link>
-              <Link to='/about'>
-                <button className='headerBtn'>About</button>
-              </Link>
-            </div>
-            <div className='headerRight'>
+            <div className='header'>
+              <div className='headerLeft'>
+                <Link className='headerLogoParent' to='/'>
+                  <img className='headerLogo' src="https://i.ibb.co/kcBcSnq/compass-logo-inverted.png" alt="My Product Compass logo"/>
+                </Link>
+                <Link to='/'>
+                  <button className='headerBtn hideOnSmall'>Home</button>
+                </Link>
+                <Link to='/products'>
+                  <button className='headerBtn'>Products</button>
+                </Link>
+                <Link to='/devs'>
+                  <button className='headerBtn'>Devs</button>
+                </Link>
+                <Link to='/about'>
+                  <button className='headerBtn hideOnSmall'>About</button>
+                </Link>
+              </div>
+              <div className='headerRight'>
 
-              {/* Render logout if manager_id in Redux is truthy */}
-              {this.props.manager_id ? 
-                <button onClick={() => this.logout()} className='headerBtn'>logout</button> :
-                <>
-                  <Link to='/register'>
-                    <button className='headerBtn'>Signup</button>
-                  </Link>
-                  <Link to='/login'>
-                    <button className='headerBtn'>Login</button>
-                  </Link>
-                </>
-              }
+                {/* Render logout if manager_id in Redux is truthy */}
+                {this.props.manager_id ? 
+                  <button onClick={() => this.logout()} className='headerBtn'>logout</button> :
+                  <>
+                    <Link to='/register'>
+                      <button className='headerBtn hideOnSmall'>Signup</button>
+                    </Link>
+                    <Link to='/login'>
+                      <button className='headerBtn'>Login</button>
+                    </Link>
+                  </>
+                }
+              </div>
             </div>
           </Router>
         </header>
