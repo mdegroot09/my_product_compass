32fb38d4510e327f0eabf9c7a5523ae5f0fea132
Mike De Groot <mdegroot09@gmail.com>
2019-05-10 14:10:30 -0600

    added styling to Dev

--- src/Components/Dev/Dev.js
+++ src/Components/Dev/Dev.js
@@ -45,19 +45,18 @@ class Dev extends Component {
       if (currentProduct === task.product_id){
         return (
           <div key={i}>
-            <p>Task: {task.name ? task.name : 'None'}</p>
-            <p>Notes: {task.notes ? task.notes : 'None'}</p>
-            <p>Due Date: {task.due_date ? task.due_date : 'None'}</p>
+            <h3 className='task' id='taskTabRight'>Task: {task.name ? task.name : 'None'}</h3>
+            <h3 className='task' id='taskTabRight'>Notes: {task.notes ? task.notes : 'None'}</h3>
+            <h3 className='task' id='taskTabRight'>Due Date: {task.due_date ? task.due_date : 'None'}</h3>
           </div>
         )
       } else {
         currentProduct = task.product_id
         return (
-          <div key={i}>
-            <h3>{task.productname}</h3>
-            <p>Task: {task.name ? task.name : 'None'}</p>
-            <p>Notes: {task.notes ? task.notes : 'None'}</p>
-            <p>Due Date: {task.due_date ? task.due_date : 'None'}</p>
+          <div className='tasksListDiv' key={i}>
+            <h2 className='productName' id='tabRight'>{task.productname}</h2>
+            <h3 className='task' id='taskTabRight'>Task: {task.name ? task.name : 'None'}</h3>
+            <h3 className='task' id='taskTabRight'>Notes: {task.notes ? task.notes : 'None'}</h3>
           </div>
         )
       }
@@ -65,18 +64,23 @@ class Dev extends Component {
     let dev = this.props.dev[0]
     let dev_id = this.props.match.params.id
     return (
-      <>
-        <p>Dev</p>
-        <div className='devTasks'>
-          <p>Developer: {dev.first_name} {dev.last_name}</p>
-          <p>Title: {dev.title}</p>
+      <div className='productsMain'>
+        <h1 className='allProducts'>{dev.first_name}'s Tasks</h1>
+        <div className='productsSubMain'>
+          <div className='productsDetails'>
+
+            <h1 className='task'>{dev.first_name} {dev.last_name}</h1>
+            <div className='productsBtns'>
+              <Link to={`/devs/update/${dev.dev_id}`}>
+                <button className='getStarted editDelete'>Edit</button>
+              </Link>
+              <button className='getStarted editDelete delete' onClick={() => this.deleteDev(dev_id)}>Delete</button>
+            </div>
+          </div>
+          <h2 className='task' id='devSubtitle'>Title: {dev.title}</h2>
           {devTasks}
-          <button onClick={() => this.deleteDev(dev_id)}>Delete</button>
-          <Link to={`/devs/update/${dev.dev_id}`}>
-            <button>Edit</button>
-          </Link>
         </div>
-      </>
+      </div>
     )
   }
 }
